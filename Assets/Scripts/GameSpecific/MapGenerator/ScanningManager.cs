62ac50a4e4eee45d3c30ec0fc994ebd6ee4e4406
Konstantin Kraus <konstantin_kraus@t-online.de>
2019-01-27 12:55:50 +0100

    Now taking over control

--- Assets/Scripts/GameSpecific/MapGenerator/ScanningManager.cs
+++ Assets/Scripts/GameSpecific/MapGenerator/ScanningManager.cs
@@ -5,38 +5,41 @@ using UnityEngine;
 
 
 /* Adapted from https://medium.com/southworks/how-to-use-spatial-understanding-to-query-your-room-with-hololens-4a6192831a6f */
-public class ScanningManager : MonoBehaviour, IInputClickHandler {
+public class ScanningManager : MonoBehaviour {
 
-    [Tooltip("3D Text for displaying scanning instructions")]
-	[SerializeField] private TextMesh InstructionTextMesh;
+    [Tooltip("List of spawnable enemies. Sort by level!") , SerializeField]
+    private GameObject[] Enemies;
 
-    public Transform FloorPrefab;
+    private uint CurrentLevel = 0; // use as index for enemies
 
-    [Tooltip("List of all spawnable enemies")]
-    [SerializeField] private GameObject[] SpawnableEnemies;
+    [Tooltip("Max number of possible positions returned"), Range(1, 64), SerializeField]
+    private int MaxResultCount = 32;
 
-    [Tooltip("List of all spawnable NPCs")]
-    [SerializeField] private GameObject[] SpawnableNPCs;
+    [Tooltip("GameObjects to disable when scanning has finsihed!"), SerializeField]
+    private GameObject[] ToDisable;
 
-    [Tooltip("Set maximum number of results returned for a spatial query")]
-    [SerializeField] private uint MaxResultCount = 32;
-
-    void Start()
+   void Start()
     {
-        InputManager.Instance.PushFallbackInputHandler(this.gameObject);
-        SpatialUnderstanding.Instance.RequestBeginScanning();
         SpatialUnderstanding.Instance.ScanStateChanged += ScanStateChanged;
     }
 
     private void ScanStateChanged()
-    {
-        if (SpatialUnderstanding.Instance.ScanState == SpatialUnderstanding.ScanStates.Scanning)
+    {   
+        if (SpatialUnderstanding.Instance.ScanState == SpatialUnderstanding.ScanStates.Done)
         {
-            LogSurfaceState();
-        }
-        else if (SpatialUnderstanding.Instance.ScanState == SpatialUnderstanding.ScanStates.Done)
-        {
-            InstantiateObjectOnFloor();
+            Debug.Log("ScanningManager: Scan complete!");
+
+            // Disable MRTK AppState script (this.IsTheCaptainNow = true)
+            GetComponent<HoloToolkit.Examples.SpatialUnderstandingFeatureOverview.AppState>().enabled = false;
+            Debug.Log("ScanningManager: Disabled MRTK AppState");
+            
+            // Disable all elements in ToDisable
+            foreach(GameObject g in ToDisable) {
+                Debug.Log("ScanningManager: Disabled " + g.name);
+                g.SetActive(false);
+            }
+
+            // Set Managers -> SpatialUnderstanding -> SUCM.MeshMaterial to None (prev. SpatialUnderstandingSurface)
         }
     }
 
@@ -47,45 +50,7 @@ public class ScanningManager : MonoBehaviour, IInputClickHandler {
 
     void Update()
     {
-        switch (SpatialUnderstanding.Instance.ScanState)
-        {
-            case SpatialUnderstanding.ScanStates.None:
-                InstructionTextMesh.text = "State: No Scanning";
-                break;
-            case SpatialUnderstanding.ScanStates.ReadyToScan:
-                InstructionTextMesh.text = "State: Ready To Scan";
-                break;
-            case SpatialUnderstanding.ScanStates.Scanning:
-                LogSurfaceState();
-                break;
-            case SpatialUnderstanding.ScanStates.Finishing:
-                InstructionTextMesh.text = "State: Finishing Scan";
-                break;
-            case SpatialUnderstanding.ScanStates.Done:
-                InstructionTextMesh.text = "State: Scan Finished";
-                break;
-            default:
-                break;
-        }
-    }
-
-    private void LogSurfaceState()
-    {
-        IntPtr statsPtr = SpatialUnderstanding.Instance.UnderstandingDLL.GetStaticPlayspaceStatsPtr();
-        if (SpatialUnderstandingDll.Imports.QueryPlayspaceStats(statsPtr) != 0)
-        {
-            var stats = SpatialUnderstanding.Instance.UnderstandingDLL.GetStaticPlayspaceStats();
-            this.InstructionTextMesh.text = string.Format("TotalSurfaceArea: {0:0.##}\nWallSurfaceArea: {1:0.##}\nHorizSurfaceArea: {2:0.##}", 
-                                                          stats.TotalSurfaceArea, stats.WallSurfaceArea, stats.HorizSurfaceArea);
-        }
-    }
-
-    public void OnInputClicked(InputClickedEventData eventData)
-    {
-        InstructionTextMesh.text = "Requested Finish Scan";
-        Debug.Log("Clicked!");
-
-        SpatialUnderstanding.Instance.RequestFinishScan();
+       
     }
 
     private void InstantiateObjectOnFloor()
@@ -101,13 +66,7 @@ public class ScanningManager : MonoBehaviour, IInputClickHandler {
 
         if (locationCount > 0)
         {
-            Instantiate(this.FloorPrefab, _resultsTopology[0].position, Quaternion.LookRotation(_resultsTopology[0].normal, Vector3.up));
-
-            this.InstructionTextMesh.text = "Spawned something";
-        }
-        else
-        {
-            this.InstructionTextMesh.text = "No suitable spawn location!";
+            Instantiate(Enemies[CurrentLevel], _resultsTopology[0].position, Quaternion.LookRotation(_resultsTopology[0].normal, Vector3.up));
         }
 }
 }
